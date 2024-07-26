5ec3c560c88f80abac1c416c1e7ed576e7beabb9
athit <athit0900@gmail.com>
2024-07-26 08:44:01 +0700

    fix api

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,21 +1,23 @@
 // app/api/route.js
-import { NextResponse } from 'next/server';
 import { Client } from 'pg';
 import dotenv from 'dotenv';
-
 dotenv.config();
-
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
 });
-
 client.connect();
-
 export async function GET() {
   try {
-    const result = await client.query('SELECT * FROM tbl_student');
-    return NextResponse.json(result.rows);
-  } catch (err) {
-    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+    const result = await client.query('SELECT * FROM tbl_users');
+    return new Response(JSON.stringify(result.rows), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
